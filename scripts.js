730e2dfd7593cfd1cb02eaca1ed4d1a3f627c777
Lanaya <Jeevankarki31@gmail.com>
2021-06-11 07:11:02 +0545

    Calculator

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+
+
+const numberButtons = document.querySelectorAll(".numbers");
+const operationButtons = document.querySelectorAll(".operation");
+const equalsButton = document.querySelector(".equals");
+const deleteButton = document.querySelector(".delete");
+const allClearButton = document.querySelector(".all-clear");
+const previousOperandTextElement = document.querySelector(".prev-oprnd");
+const currentOperandTextElement = document.querySelector(".cur-oprnd");
+const adderBtn = document.querySelector('.add');
+const minusBtn = document.querySelector('.minus');
+const divideBtn = document.querySelector('.divide');
+
